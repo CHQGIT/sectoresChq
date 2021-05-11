e53499f4623c726b59908eeacb2e68f7df2a0b0d
Evelyn Hernández <darklilium@users.noreply.github.com>
2021-05-11 17:24:31 -0500

    Update config.js
    AccountChanged

--- src/services/config.js
+++ src/services/config.js
@@ -21,8 +21,8 @@ const env = {
 
 var conf = ( () => {
   var credentials = {
-    user: 'vialactea\\usrgis',
-    pass: 'N3L4y5HZ'
+    user: '',
+    pass: ''
   }
 
   return () => {return credentials}
